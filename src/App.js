d1711ed006ea39e7eca031bea74cf6f92e7b5c0f
owencyr <cyroc@dukes.jmu.edu>
2019-02-11 12:00:10 -0500

    working on input fields

--- src/App.js
+++ src/App.js
@@ -4,7 +4,36 @@ import Header from './components/Header'
 import FilterOptions from './components/FilterOptions'
 import List from './components/List'
 
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params).map(
+    key => (key) ? `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}` : ''
+  );
+  return queryItems.join('&');
+}
 class App extends Component {
+  state = {
+    searchTerm: null,
+    printType: null,
+    bookType: null
+  }
+  
+  componentDidMount() {
+    this.getBookResults();
+  }
+ 
+  getBookResults() {
+    const BASE_URL = `https://www.googleapis.com/books/v1/volumes?q=`
+    const searchQuery = `${this.state.searchTerm}`
+    const printType = `${this.state.printType}`
+    const bookType = `${this.state.bookType}`
+    const params = {searchQuery, printType, bookType}
+    
+    
+
+    fetch(BASE_URL
+  }
+  // const BASE_URL = 
+
   render() {
     return (
       <div className="application-container">
